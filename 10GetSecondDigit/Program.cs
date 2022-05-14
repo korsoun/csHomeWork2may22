d85d56636b676b362d050341b7d4cd3325bf65ee
alexkors <korsun55555@mail.ru>
2022-05-14 18:53:03 +0700

    1

---
+++ 10GetSecondDigit/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10
+// Получить трехзначное число и вывести вторую цифру
+
+Console.WriteLine("Введите трехзначное число:");
+int number = int.Parse(Console.ReadLine());
+
+int secondDigit = GetSecondDigit(number);
+Console.Write("Вторая цифра этого числа - " + secondDigit);
+
+int GetSecondDigit(int number) {
+    int result = (number % 100) / 10;
+    return result;
+}
