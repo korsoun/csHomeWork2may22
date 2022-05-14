55b9f0977d157dbab92b3affacee46553d03720b
alexkors <korsun55555@mail.ru>
2022-05-14 19:24:57 +0700

    решение второй задачи

--- 13GetThirdDigit/Program.cs
+++ 13GetThirdDigit/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13
+// вывести третью цифру заданного числа или сообщить, что ее нет
+
+Console.WriteLine("Введите целое неотрицательное число");
+int numFromCons = Convert.ToInt32(Console.ReadLine());
+
+GetThirdDigit(numFromCons);
+
+void GetThirdDigit(int numFromCons) 
+{
+    if(numFromCons / 100 != 0) {
+        int ThirdDigit = (numFromCons % 1000) / 100;
+        Console.WriteLine("Третья цифра числа - " + ThirdDigit + ".");
+    }
+else {
+    Console.WriteLine("В этом числе нет третьей цифры.");
+}
+}
