9aa3e4177b828e4001def5c7c63fc62b231420df
alexkors <korsun55555@mail.ru>
2022-05-17 22:06:33 +0700

    новое решение задачи 13

--- 10GetSecondDigit/Program.cs
+++ 10GetSecondDigit/Program.cs
@@ -1,12 +1,16 @@
 ﻿// Задача 10
 // Получить трехзначное число и вывести вторую цифру
 
+//получение числа и перевод в int для переменной
 Console.WriteLine("Введите трехзначное число:");
-int number = int.Parse(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 
+//присваивание переменной secondDigit результата выполнения метода
 int secondDigit = GetSecondDigit(number);
 Console.Write("Вторая цифра этого числа - " + secondDigit);
 
+
+//метод для нахождения второй цифры
 int GetSecondDigit(int number) {
     int result = (number % 100) / 10;
     return result;
--- 13GetThirdDigit/Program.cs
+++ 13GetThirdDigit/Program.cs
@@ -6,13 +6,21 @@ int numFromCons = Convert.ToInt32(Console.ReadLine());
 
 GetThirdDigit(numFromCons);                                                     //вызов метода GetThirdDigit c результатом обработки введенного числа  
 
-void GetThirdDigit(int numFromCons)                                             // описание метода GetThirdDigit
+void GetThirdDigit(int numFromCons)                              // описание метода GetThirdDigit
 {
-    if(numFromCons / 100 != 0) {                
-        int ThirdDigit = (numFromCons % 1000) / 100;                            //если есть целая часть от деления на 100, т.е. число трех- или более значное,
-        Console.WriteLine("Третья цифра числа - " + ThirdDigit + ".");          //то отбрасываем разряды старше сотых, затем отбрасываем разряды младше сотых
-    }                                                                           //и выводим строку с указанием цифры в разряде сотых
-else {
-    Console.WriteLine("В этом числе нет третьей цифры.");                       //если целой части при делении на 100 нет, значит, число меньше ста и разряда сотых в нем нет
-}
+    int denom;                                                   // через denom и frontDigit идет проверка разрядности числа, 
+    int frontDigit = 10;
+    if (numFromCons / 100 != 0)                                  // проверка на недопустимый ввод. расчет ведется при вводе трех- и более значном числе
+    {
+        for (denom = 100; frontDigit > 9; denom = denom * 10)    // цикл поиска разрядности. достижение frontDigit значения 9 и менее говорит о том, что frontDigit является последней цифрой при движении справа налево
+        {
+            frontDigit = numFromCons / denom;
+        }    
+        numFromCons = (numFromCons / (denom / 1000)) % 10;       // отбросить три ноля у denom. поделить число на denom и взять целую часть, у которой искомая цифра является последней. взять остаток деления целой части на 10               
+        Console.WriteLine("Третья цифра числа - " + numFromCons + ".");          
+    }                                                                           
+        else
+    {
+        Console.WriteLine("В этом числе нет третьей цифры.");                       //если целой части при делении на 100 нет, значит, число меньше ста и разряда сотых в нем нет
+    }
 }
